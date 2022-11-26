8909c837522419c52887afbc582dc27b8b5bcbb9
Sebastianjohansson123 <johansson.sebastian.92@hotmail.com>
2022-11-26 20:03:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+  setAllConstants();
+  addEventListeners();
+}
+
+let hamburger;
+let timeoutRef;
+let burgercontent;
+setInterval(addChar, 100);
+
+function setAllConstants() {
+  hamburger = document.getElementById("hamburger-div")
+  burgercontent = document.querySelector(".hamburger-div ul");
+}
+
+function addEventListeners() {
+  hamburger.addEventListener("click", loadHamburgerMenu);
+
+}
+
+function loadHamburgerMenu() {
+  if (hamburger.classList.contains("hamburger-fullscreen")) {
+    hamburger.classList.remove("hamburger-fullscreen")
+    burgercontent.classList.add("hidden");
+  }
+  else {
+    hamburger.classList.add("hamburger-fullscreen")
+    timeoutRef = setTimeout(showBurgerContent, 500);
+  }
+}
+function showBurgerContent() {
+  burgercontent.classList.remove("hidden");
+}
+
